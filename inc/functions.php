3b4dbb0f8627766ccd20b681a05c37d5047112a2
RoyHridoy <royhridoy1428@gmail.com>
2018-12-11 15:28:23 +0600

    Added add new student and basic edit functionality but not completed edit functionality

--- inc/functions.php
+++ inc/functions.php
@@ -1,77 +1,175 @@
 <?php
-define( "DB_NAME" , "/opt/lampp/htdocs/CRUD-Student-Management/data/data.txt" );
+define("DB_NAME", "/opt/lampp/htdocs/CRUD-Student-Management/data/data.txt");
 
-    function seed(){
-        
-        $data = array(
-            array(
-                'id' => 1,
-                'fname' => 'Rajib',
-                'lname' => 'Ahmed',
-                'class' => '8',
-                'roll' => 15,
-            ),
-            array(
-                'id' => 2,
-                'fname' => 'Mihir',
-                'lname' => 'Chowdhury',
-                'class' => '8',
-                'roll' => 13,
-            ),
-            array(
-                'id' => 3,
-                'fname' => 'Safa',
-                'lname' => 'Kabir',
-                'class' => '8',
-                'roll' => 10,
-            ),
-            array(
-                'id' => 4,
-                'fname' => 'Khalil',
-                'lname' => 'Ahmed',
-                'class' => '8',
-                'roll' => 9,
-            ),
-            array(
-                'id' => 5,
-                'fname' => 'Karim',
-                'lname' => 'Khan',
-                'class' => '8',
-                'roll' => 5,
-            )
-        );
+function seed(){
 
-        $serializeData = serialize($data);
-        if (is_writable(DB_NAME)) {
-            file_put_contents(DB_NAME,$serializeData,LOCK_EX);
-        }
+    $data = array(
+        array(
+            'id'    => 1,
+            'fname' => 'Rajib',
+            'lname' => 'Ahmed',
+            'class' => '8',
+            'roll'  => 15,
+        ),
+        array(
+            'id'    => 2,
+            'fname' => 'Mihir',
+            'lname' => 'Chowdhury',
+            'class' => '8',
+            'roll'  => 13,
+        ),
+        array(
+            'id'    => 3,
+            'fname' => 'Safa',
+            'lname' => 'Kabir',
+            'class' => '8',
+            'roll'  => 10,
+        ),
+        array(
+            'id'    => 4,
+            'fname' => 'Khalil',
+            'lname' => 'Ahmed',
+            'class' => '8',
+            'roll'  => 9,
+        ),
+        array(
+            'id'    => 5,
+            'fname' => 'Karim',
+            'lname' => 'Khan',
+            'class' => '8',
+            'roll'  => 5,
+        )
+    );
+
+    $serializeData = serialize($data);
+    if (is_writable(DB_NAME)) {
+        file_put_contents(DB_NAME, $serializeData, LOCK_EX);
     }
+}
 
-    function displayData(){
-        $unserializedData = file_get_contents(DB_NAME);
-        $data = unserialize($unserializedData);
+function displayData(){
+    $unserializedData = file_get_contents(DB_NAME);
+    $data = unserialize($unserializedData);
+    if (!empty($data)) :
         ?>
         <table class="table table-striped table-bordered">
             <thead class="thead-dark">
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Class</th>
-                    <th>Roll</th>
-                    <th>Action</th>
-                </tr>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Class</th>
+                <th>Roll</th>
+                <th>Action</th>
+            </tr>
             </thead>
             <?php
+
             foreach ($data as $singleData) {
                 echo "<tr>";
                 echo "<td>{$singleData['id']} </td>";
                 echo "<td>{$singleData['fname']} {$singleData['lname']}</td>";
                 echo "<td>{$singleData['class']}</td>";
                 echo "<td>{$singleData['roll']}</td>";
-                echo "<td> <a href='#'>Edit</a> | <a href='#'>Delete</a> </td>";
+                echo "<td> <a href='index.php?task=edit&id={$singleData['id']}'>Edit</a> | <a href='index.php?task=delete&id={$singleData['id']}'>Delete</a> </td>";
                 echo "</tr>";
             }
             ?>
         </table>
-<?php
+    <?php
+    else: echo "<div class=\"alert alert-warning\" role=\"alert\">There is no data available to see. Please insert some data.</div>";
+    endif;
+}
+
+function addNewStudent($fname,$lname,$roll,$class){
+
+    $data =    file_get_contents(DB_NAME);
+    $students = unserialize($data);
+    $found = false;
+    foreach ($students as $student) {
+        if ($student['roll'] == $roll && $student['class'] == $class){
+            $found = true;
+        }
     }
+
+    if (!$found){
+        $student = array(
+            'id'    => count($students) +1,
+            'fname' => $fname,
+            'lname' => $lname,
+            'roll'  => $roll,
+            'class' => $class
+        );
+        array_push($students,$student);
+
+        $serializeData = serialize($students);
+
+        if (is_writable(DB_NAME)) {
+            file_put_contents(DB_NAME, $serializeData, LOCK_EX);
+        }
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
+function getStudent($id){
+    $unserializedData = file_get_contents(DB_NAME);
+    $students = unserialize($unserializedData);
+
+    foreach ($students as $student) {
+        if ($student['id'] == $id){
+            return $student;
+        }
+    }
+    return false;
+}
+
+
+function updateStudent($fname,$lname,$roll,$class,$id){
+    $unserializedData = file_get_contents(DB_NAME);
+    $students = unserialize($unserializedData);
+
+    $students[$id-1]['fname'] = $fname;
+    $students[$id-1]['lname'] = $lname;
+    $students[$id-1]['roll'] = $roll;
+    $students[$id-1]['class'] = $class;
+
+    $serializeData = serialize($students);
+
+    if (is_writable(DB_NAME)) {
+        file_put_contents(DB_NAME, $serializeData, LOCK_EX);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -2,51 +2,171 @@
 
 <?php
 $task = $_GET['task'] ?? 'report';
-// $task = "report";
+$error = $_GET['error'] ?? '0';
+$fname = '';
+$lname = '';
+$roll = '';
+$class = '';
+if (isset($_POST['submit'])) {
+    $fname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+    $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
+    $roll = filter_input(INPUT_POST, 'roll', FILTER_SANITIZE_STRING);
+    $class = filter_input(INPUT_POST, 'class', FILTER_SANITIZE_STRING);
+    $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+
+    if ($id) {
+        // Update the student
+        if ($fname != '' && $lname != '' && $roll != '' && $class != '') {
+            updateStudent($fname,$lname,$roll,$class,$id);
+            header('location: index.php?task=report');
+        }
+    } else {
+        // If not found id then create the student
+        if ($fname != '' && $lname != '' && $roll != '' && $class != '') {
+            $result = addNewStudent($fname, $lname, $roll, $class);
+            if ($result) {
+                header("location: index.php?task=report");
+            } else {
+                $error = 1;
+            }
+        }
+    }
+}
 
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <link rel="stylesheet" href="./inc/templates/style.css">
 
     <title>CRUD - Student Management</title>
-  </head>
-  <body>
+</head>
+<body>
 
-    <?php include_once "./inc/templates/nav.php"; ?>
+<?php include_once "./inc/templates/nav.php"; ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="contents">
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="contents">
+
+                <?php if ('seed' == $task) {
+                    seed();
+                    echo "<h2 class='page-title'> Seed Data </h2>";
+                    echo "<div class=\"row\"><div class=\"col-8 offset-2\"><div class=\"alert alert-success text-center\" role=\"alert\">Data Seeding is Successfully Completed!</div></div></div>";
+                }
+
+                if ('report' == $task) {
+                    echo "<h2 class='page-title'> All students </h2>";
+                    displayData();
+                }
+                ?>
+
+                <?php
+                if ('1' == $error) {
+                    echo "<div class='alert alert-danger text-center'>Roll Number Dublicate</div>";
+                }
+                ?>
+
+                <?php if ('add' == $task) : ?>
+                    <?php echo "<h2 class='page-title'> Add new student </h2>"; ?>
+                    <div class="row">
+                        <div class="col-8 offset-2">
+                            <form action="index.php?task=add" method="POST">
+                                <div class="form-group">
+                                    <label for="fname">First Name</label>
+                                    <input type="text" name="fname" id="fname" class="form-control"
+                                           value="<?php echo $fname; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="lname">Last Name</label>
+                                    <input type="text" name="lname" id="lname" class="form-control"
+                                           value="<?php echo $lname; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="class">Class</label>
+                                    <input type="number" name="class" id="class" class="form-control"
+                                           value="<?php echo $class; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="roll">Roll Number</label>
+                                    <input type="number" name="roll" id="roll" class="form-control"
+                                           value="<?php echo $roll; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary" name="submit">Save</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+
+                <?php if ('edit' == $task) :
+                    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+                    $student = getStudent($id);
+                    if ($student) :
+                        ?>
+                        <?php echo "<h2 class='page-title'> Edit student </h2>"; ?>
+                        <div class="row">
+                            <div class="col-8 offset-2">
+                                <form action="" method="POST">
+                                    <input type="hidden" value="<?php echo $id; ?>" name="id">
+                                    <div class="form-group">
+                                        <label for="fname">First Name</label>
+                                        <input type="text" name="fname" id="fname" class="form-control"
+                                               value="<?php echo $student['fname']; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="lname">Last Name</label>
+                                        <input type="text" name="lname" id="lname" class="form-control"
+                                               value="<?php echo $student['lname']; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="class">Class</label>
+                                        <input type="number" name="class" id="class" class="form-control"
+                                               value="<?php echo $student['class']; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="roll">Roll Number</label>
+                                        <input type="number" name="roll" id="roll" class="form-control"
+                                               value="<?php echo $student['roll']; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary" name="submit">Update</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     <?php
-                        if ('seed' == $task ) {
-                            seed();
-                            echo "Data Seeding Successfully";
-                        }
-                        if ('report' == $task ) {
-                            echo "<h2 class='page-title'> All students </h2>";
-                            displayData();
-                        }
-                    ?>
-                </div>
+                    endif;
+                endif;
+                ?>
+
             </div>
         </div>
     </div>
+</div>
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
 </html>
